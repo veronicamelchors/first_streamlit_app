3c46803d49c6f4074da377a2d10a1c6e68191241
veronicamelchors <42653540+veronicamelchors@users.noreply.github.com>
2022-06-21 15:03:47 -0700

    Update streamlit_app.py
    Change title to header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach, Rocket Smoothie')
-streamlit.title('Hard-Boild Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, Rocket Smoothie')
+streamlit.text('Hard-Boild Free-Range Egg')
 
 
 
