a336cc4ab956d0766ead0ae7caf97c97ebc8f980
veronicamelchors <42653540+veronicamelchors@users.noreply.github.com>
2022-06-22 12:08:47 -0700

    Update streamlit_app.py
    add new emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My Parents New Healthy Diner - ')
 
 streamlit.header('Breakfast Favorites')
 
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal 🍔')
 streamlit.text('🥗 Kale, Spinach, Rocket Smoothie')
 streamlit.text('🐔 Hard-Boild Free-Range Egg')
 streamlit.text('🍞🥑 Avocado Toast')
